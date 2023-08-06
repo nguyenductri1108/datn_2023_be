1f55f60e96a1aba004affc288ec5120951096c81
Trí Nguyễn Đức <nguyenductri1108.thd@gmail.com>
2023-08-06 17:45:17 +0700

    commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/web_book");
+        await mongoose.connect(
+            "mongodb+srv://nguyenductri1108thd3:tri1234@cluster0.ckc79eo.mongodb.net/?retryWrites=true&w=majority"
+        );
         console.log("Connected to MongoDB");
     } catch (e) {
-        console.log("Error connecting to MongoDB");
+        console.log("Error connecting to MongoDB", e);
     }
 }
 
